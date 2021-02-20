aa6d2966ec0ba7b0b48e9d8ed932f85be22688e7
Madeleine <madeleine.schonemann@gmail.com>
2021-02-20 16:30:16 +0100

    Update podcast episode player

--- src/components/routes/Episodes/index.js
+++ src/components/routes/Episodes/index.js
@@ -1,38 +1,33 @@
 import React from "react";
 import Section from "../../common/Section";
 import styles from "./episodes.module.scss";
-import ReactAudioPlayer from "react-audio-player";
 
 const Episodes = () => {
   const episodes = [
     {
-      url: "https://media.transistor.fm/726e048b/ba70f6b0.mp3",
-      name: "7. Kan du SOLID?",
+      id: "b032e11f",
     },
     {
-      url: "https://media.transistor.fm/91c99a44/3fe33391.mp3",
-      name: "6. Ta dig i mål!",
+      id: "726e048b",
     },
     {
-      url: "https://media.transistor.fm/2a867e9c/020f204a.mp3",
-      name: "5. 2020 i backspegeln",
+      id: "91c99a44",
+    },
+    {
+      id: "2a867e9c",
     },
 
     {
-      url: "https://media.transistor.fm/92ca9be1/eb5d91de.mp3",
-      name: "4. Vilse i tangentbordsträsket",
+      id: "92ca9be1",
     },
     {
-      url: "https://media.transistor.fm/b5d93ff5/c867cf2d.mp3",
-      name: "3. Julkalendern",
+      id: "b5d93ff5",
     },
     {
-      url: "https://media.transistor.fm/0698b342/dbf9daaa.mp3",
-      name: "2. Fruktade programmeringsspråk?!",
+      id: "0698b342",
     },
     {
-      url: "https://media.transistor.fm/c7bd8c4a/df40d647.mp3",
-      name: "1. $podd init",
+      id: "c7bd8c4a",
     },
   ];
 
@@ -43,7 +38,15 @@ const Episodes = () => {
           return (
             <div className={styles.episode} key={index}>
               <h2>{episode.name}</h2>
-              <ReactAudioPlayer src={episode.url} controls></ReactAudioPlayer>
+
+              <iframe
+                width="100%"
+                height="180"
+                frameborder="no"
+                scrolling="no"
+                seamless
+                src={`https://share.transistor.fm/e/${episode.id}/dark`}
+              ></iframe>
             </div>
           );
         })}
