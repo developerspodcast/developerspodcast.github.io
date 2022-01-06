9b51307f062d771cbcf776a4c28ccb708aab602c
Madeleine <madeleine.schonemann@gmail.com>
2022-01-06 18:52:55 +0100

    Change header to backgroundImage

--- src/components/common/Hero/index.js
+++ src/components/common/Hero/index.js
@@ -4,11 +4,10 @@ import background from "./background.svg";
 
 const Hero = () => {
   return (
-    <div className={styles.hero}>
-      <img
-        src={background}
-        alt="Illustration av Sofia och Madde framför en rosa cirkel. Sofia är blond, har en grön tröja och håller i en kopp med ett git-träd. Madeleine är brunhårig, har en gul tröja och håller i sin dator med en ananaslogga."
-      ></img>
+    <div
+      className={styles.hero}
+      style={{ backgroundImage: `url(${background})` }}
+    >
       <div className={styles.tagline}>
         <h1>Developers!</h1>
         <h2>En podd om mer än bara kod</h2>
