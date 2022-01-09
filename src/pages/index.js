a48c39a80d5b9b6858411b51c263e31afd788102
Madeleine <madeleine.schonemann@gmail.com>
2022-01-09 16:05:03 +0100

    Update texts

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,19 +12,13 @@ const IndexPage = () => {
         <div className="container">
           <h2 className="mb-1">Om podden</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nec
-            pellentesque purus. Nullam feugiat lorem id fringilla luctus. Donec
-            at ex risus. Morbi venenatis massa porta nisi efficitur, id volutpat
-            neque laoreet. Aenean malesuada luctus nisi eu imperdiet. Curabitur
-            ut congue turpis. Morbi vel urna orci. Aliquam elementum, elit eu
-            sodales efficitur, sapien ante pulvinar justo, in interdum magna
-            velit non tellus. Nam vel ligula mattis, tempor odio quis, fermentum
-            tellus. In non mi sed lorem interdum placerat eu sit amet lorem.
-            Donec eleifend justo vel auctor mattis. Donec blandit diam mauris,
-            nec pretium lectus condimentum id. Duis sit amet efficitur lacus.
-            Aliquam tincidunt et sapien sit amet gravida. Vivamus eleifend ipsum
-            at velit ultrices, sit amet scelerisque urna hendrerit. Proin semper
-            posuere eros, nec mollis tellus congue non.
+            I podden Developers! får ni följa med oss, Sofia och Madde, på allt
+            inom mjukvaruutveckling. Vi arbetar som utvecklare men podden
+            handlar om mer än bara kod!
+          </p>
+          <p>
+            Vi träffar spännade gäster, testar nya teknologier, söker
+            inspiration och tar upp aktuella ämnen.
           </p>
         </div>
         <TwoSection
@@ -48,15 +42,9 @@ const IndexPage = () => {
           secondSection={<img src={madde}></img>}
           firstSection={
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-              nec pellentesque purus. Nullam feugiat lorem id fringilla luctus.
-              Donec at ex risus. Morbi venenatis massa porta nisi efficitur, id
-              volutpat neque laoreet. Aenean malesuada luctus nisi eu imperdiet.
-              Curabitur ut congue turpis. Morbi vel urna orci. Aliquam
-              elementum, elit eu sodales efficitur, sapien ante pulvinar justo,
-              in interdum magna velit non tellus. Nam vel ligula mattis, tempor
-              odio quis, fermentum tellus. In non mi sed lorem interdum placerat
-              eu sit amet lorem. Donec eleifend justo vel auctor mattis.
+              Madde är 30 år, bor i Malmö och jobbar som fullstackutvecklare sen
+              2014. Hennes stora passion är att forma team som tillsammans
+              jobbar så effektivt som möjligt.
             </p>
           }
         ></TwoSection>
