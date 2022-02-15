1feb9ddc63b10b78e6b14c66d912b5835e58989d
Madeleine <madeleine.schonemann@gmail.com>
2022-02-15 19:42:02 +0100

    Increase margin in epsiode description

--- src/components/routes/Episodes/Episode.js
+++ src/components/routes/Episodes/Episode.js
@@ -3,16 +3,24 @@ import Button from "../../common/Button";
 import styles from "./episode.module.css";
 
 const Episode = ({ episode, ...props }) => {
-    const [showDescription, setShowDescription] = useState(false);
-    const toggleDisplay = () => setShowDescription(!showDescription);
-    return <div className={styles.episode} {...props}>
-        <div className={styles.header}>
-            <h2 className={styles.title}>{episode.title}</h2>
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: episode.embedCode }}></div>
-        <Button small onClick={toggleDisplay}>{!showDescription ? 'Avsnittsinfo' : 'Stäng'}</Button>
-        {showDescription && <div className={styles.description} dangerouslySetInnerHTML={{ __html: episode.description }}></div>}
+  const [showDescription, setShowDescription] = useState(false);
+  const toggleDisplay = () => setShowDescription(!showDescription);
+  return (
+    <div className={styles.episode} {...props}>
+      <div className={styles.header}>
+        <h2 className={styles.title}>{episode.title}</h2>
+      </div>
+      <div dangerouslySetInnerHTML={{ __html: episode.embedCode }}></div>
+      <Button small onClick={toggleDisplay}>
+        {!showDescription ? "Avsnittsinfo" : "Stäng"}
+      </Button>
+      {showDescription && (
+        <div
+          className={styles.description}
+          dangerouslySetInnerHTML={{ __html: episode.description }}
+        ></div>
+      )}
     </div>
-}
-export default Episode
-
+  );
+};
+export default Episode;
